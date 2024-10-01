bb99628c655642ea69d2453e303b3f3b89f58e2b
Mansa <luis.sanchez77@unach.mx>
2024-10-01 12:34:43 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto 007 Arduino LED/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Proyecto_007_Arduino_LED
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_Encender = new System.Windows.Forms.Button();
+            this.btn_Apagar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Encender
+            // 
+            this.btn_Encender.BackColor = System.Drawing.Color.ForestGreen;
+            this.btn_Encender.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Encender.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Encender.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Encender.Location = new System.Drawing.Point(186, 108);
+            this.btn_Encender.Name = "btn_Encender";
+            this.btn_Encender.Size = new System.Drawing.Size(165, 63);
+            this.btn_Encender.TabIndex = 0;
+            this.btn_Encender.Text = "ENCENDER";
+            this.btn_Encender.UseVisualStyleBackColor = false;
+            this.btn_Encender.Click += new System.EventHandler(this.btn_Encender_Click);
+            // 
+            // btn_Apagar
+            // 
+            this.btn_Apagar.BackColor = System.Drawing.Color.Red;
+            this.btn_Apagar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Apagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Apagar.ForeColor = System.Drawing.SystemColors.Info;
+            this.btn_Apagar.Location = new System.Drawing.Point(186, 218);
+            this.btn_Apagar.Name = "btn_Apagar";
+            this.btn_Apagar.Size = new System.Drawing.Size(165, 63);
+            this.btn_Apagar.TabIndex = 1;
+            this.btn_Apagar.Text = "APAGAR";
+            this.btn_Apagar.UseVisualStyleBackColor = false;
+            this.btn_Apagar.Click += new System.EventHandler(this.btn_Apagar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(569, 393);
+            this.Controls.Add(this.btn_Apagar);
+            this.Controls.Add(this.btn_Encender);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CONTROLADOR LED";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.close_form);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Encender;
+        private System.Windows.Forms.Button btn_Apagar;
+    }
+}
+
---
+++ Proyecto 007 Arduino LED/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;//usa los puertos de la PC
+
+namespace Proyecto_007_Arduino_LED
+{
+    public partial class Form1 : Form
+    {
+        public SerialPort ArduinoPort { get; }
+        //obbtiene las puertas de comunicacion serial del PC
+
+        public Form1()
+        {
+            InitializeComponent();
+            //PUERTO [COM3]
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM3";
+            ArduinoPort.BaudRate = 9600;
+            //velocidad de baudios en el puerto serie los baudios pueden contener varios bits
+            ArduinoPort.Open();
+
+            //vinculacion de los eventos
+            this.btn_Apagar.Click += btn_Apagar_Click;
+            this.btn_Encender.Click += btn_Encender_Click;
+            this.FormClosing += close_form;
+
+        }
+
+        private void close_form(object sender, FormClosingEventArgs e)
+        {
+            if (ArduinoPort.IsOpen) ArduinoPort.Close();
+        }
+
+        private void btn_Encender_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("b");
+        }
+
+        private void btn_Apagar_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("a");
+        }
+    }
+}
